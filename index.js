49bcd947145c7b945ebb8082726d76e49844f5b0
Arezki AIT MOUHEB <ait.mouheb.arezki@gmail.com>
2019-02-21 22:27:39 +0100

    Site Web Architecture done

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let express = require('express');
+let path = require('path');
+
+let server = express();
+
+server.use(express.static(path.join(__dirname,'New Libelille')));
+
+server.get('/', function(req,res){
+    res.sendFile('/index.html', {root: __dirname })
+});
+
+server.listen(9000,function(){
+    console.log('Server listening on Port 9000');
+});
+
